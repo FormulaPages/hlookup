08f293a4f1ecfa43e5ed408aab7d5efa8977ab21
Peter Moresi <peter.moresi@gmail.com>
2015-09-12 15:42:47 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+(function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(["exports", "module", "formula-isblank"], factory);
+    } else if (typeof exports !== "undefined" && typeof module !== "undefined") {
+        factory(exports, module, require("formula-isblank"));
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory(mod.exports, mod, global.ISBLANK);
+        global.HLOOKUP = mod.exports;
+    }
+})(this, function (exports, module, _formulaIsblank) {
+    "use strict";
+
+    module.exports = HLOOKUP;
+
+    function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+    var _ISBLANK = _interopRequireDefault(_formulaIsblank);
+
+    function HLOOKUP() {
+        if (typeof needle === "undefined" || fn.ISBLANK(needle)) {
+            return null;
+        }
+
+        var index = index || 0,
+            row = table[0];
+
+        for (var i = 0; i < row.length; i++) {
+
+            if (exactmatch && row[i] === needle || row[i].toLowerCase().indexOf(needle.toLowerCase()) !== -1) {
+                return index < table.length + 1 ? table[index - 1][i] : table[0][i];
+            }
+        }
+
+        return error.na;
+    }
+});
